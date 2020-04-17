1ec820bc7f436931d7f28838bcebb6f6fab432a6
mangesh <mshanit@gmail.com>
2020-04-17 23:35:10 +0530

    Updates

---
+++ precache-manifest.cdba4b3083a09646af1e7f5532aef73a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ff97eaeb0f731050b247c0e69153814",
+    "url": "/covid_kanvas/index.html"
+  },
+  {
+    "revision": "81cc574563a2d95cd095",
+    "url": "/covid_kanvas/static/css/2.cc90cae1.chunk.css"
+  },
+  {
+    "revision": "eb5e23a7c0396628a930",
+    "url": "/covid_kanvas/static/css/main.de4e7d7a.chunk.css"
+  },
+  {
+    "revision": "81cc574563a2d95cd095",
+    "url": "/covid_kanvas/static/js/2.489c94a5.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/covid_kanvas/static/js/2.489c94a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb5e23a7c0396628a930",
+    "url": "/covid_kanvas/static/js/main.8a6cfcfd.chunk.js"
+  },
+  {
+    "revision": "88a8c854152188b6b294",
+    "url": "/covid_kanvas/static/js/runtime-main.50379750.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/covid_kanvas/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/covid_kanvas/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/covid_kanvas/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/covid_kanvas/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/covid_kanvas/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
